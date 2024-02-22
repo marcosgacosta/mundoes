cb71d70de65406117f48ef98e7833c4f53dc7819
Marcos Acosta <61573326+marcosgacosta@users.noreply.github.com>
2024-02-22 16:53:17 -0500

    Update ec2_user_data.sh

--- 00_ec2/ec2_user_data.sh
+++ 00_ec2/ec2_user_data.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Installing AWS CLI"
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 echo "Installing kubectl"
 curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.26.2/2023-03-17/bin/linux/amd64/kubectl
